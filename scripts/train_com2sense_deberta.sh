61193a17c40f1d4b950c90f1f73db8abc6188ec8
DrewLetvin <32301845+DrewLetvin@users.noreply.github.com>
2022-12-05 13:24:23 -0800

    update hyper params for train

--- scripts/train_com2sense_deberta.sh
+++ scripts/train_com2sense_deberta.sh
@@ -1,7 +1,7 @@
 DATA_DIR="datasets/com2sense"
-MODEL_TYPE="bert-base-cased"
+#MODEL_TYPE="bert-base-cased"
 #MODEL_TYPE="roberta-base"
-#MODEL_TYPE="microsoft/deberta-base"
+MODEL_TYPE="microsoft/deberta-base"
 TASK_NAME="com2sense"
 OUTPUT_DIR=${TASK_NAME}/ckpts
 
@@ -9,22 +9,22 @@ OUTPUT_DIR=${TASK_NAME}/ckpts
 CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
   --do_train \
-  --evaluate_during_training \
   --do_eval \
-  --eval_all_checkpoints \
+  --eval
   --gradient_accumulation_steps 4 \
   --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 1 \
   --learning_rate 1e-5 \
-  --max_steps 400 \
+  --max_steps 4000 \
   --max_seq_length 128 \
   --output_dir "${OUTPUT_DIR}/ckpts" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
-  --save_steps 100 \
-  --logging_steps 100 \
-  --warmup_steps 10 \
+  --save_steps 1000 \
+  --logging_steps 1000 \
+  --warmup_steps 100 \
   --eval_split "dev" \
+  --iters_to_eval 1000 2000 3000 4000 \
   --score_average_method "micro" \
   --do_not_load_optimizer \
   --overwrite_output_dir \
