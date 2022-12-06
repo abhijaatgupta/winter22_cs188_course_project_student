db9d86f5c68ab0143f32198a7905358a68779db7
DrewLetvin <32301845+DrewLetvin@users.noreply.github.com>
2022-12-05 23:58:02 -0800

    Update train_com2sense_deberta.sh

--- scripts/train_com2sense_deberta.sh
+++ scripts/train_com2sense_deberta.sh
@@ -8,7 +8,6 @@ OUTPUT_DIR=${TASK_NAME}/ckpts
 
 CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
-  --do_train \
   --do_eval \
   --gradient_accumulation_steps 4 \
   --per_gpu_train_batch_size 32 \
@@ -22,8 +21,8 @@ CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --save_steps 300 \
   --logging_steps 300 \
   --warmup_steps 100 \
-  --eval_split "dev" \
+  --eval_split "test" \
   --score_average_method "micro" \
   --do_not_load_optimizer \
-  --iters_to_eval 300 600 900 1200  \
+  --iters_to_eval 1200  \
   --overwrite_output_dir \
