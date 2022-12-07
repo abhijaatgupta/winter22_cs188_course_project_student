ff7eda286b761d1dc34c4c40efe8788d1775c8d4
DrewLetvin <32301845+DrewLetvin@users.noreply.github.com>
2022-12-07 12:22:34 -0800

    get roberta dev evals

--- scripts/train_com2sense_deberta.sh
+++ scripts/train_com2sense_deberta.sh
@@ -1,28 +1,29 @@
 DATA_DIR="datasets/com2sense"
 #MODEL_TYPE="bert-base-cased"
-#MODEL_TYPE="roberta-base"
-MODEL_TYPE="microsoft/deberta-base"
+MODEL_TYPE="roberta-base"
+#MODEL_TYPE="microsoft/deberta-base"
 TASK_NAME="com2sense"
 OUTPUT_DIR=${TASK_NAME}/ckpts
 
 
 CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
+  --do_train \
   --do_eval \
   --gradient_accumulation_steps 4 \
-  --per_gpu_train_batch_size 32 \
+  --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 1 \
-  --learning_rate 1e-4 \
-  --num_train_epochs 100.0 \
+  --learning_rate 1e-5 \
+  --max_steps 4000 \
   --max_seq_length 128 \
   --output_dir "${OUTPUT_DIR}/ckpts" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
-  --save_steps 300 \
-  --logging_steps 300 \
+  --save_steps 1000 \
+  --logging_steps 1000 \
   --warmup_steps 100 \
   --eval_split "dev" \
-  --score_average_method "micro" \
+  --score_average_method "binary" \
   --do_not_load_optimizer \
-  --iters_to_eval 300 600  \
+  --iters_to_eval 1000 2000 3000 4000 \
   --overwrite_output_dir \
